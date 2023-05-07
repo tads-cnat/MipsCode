5f71bd7e997b19479fc9d85d9a0ae222b2fae3b6
Daniela Lima <dani7fl7@gmail.com>
2023-05-07 20:10:13 -0300

    :construction: Implementando Criar Projeto

--- frontend/src/features/Home/index.tsx
+++ frontend/src/features/Home/index.tsx
@@ -1,11 +1,25 @@
+import Button from '@mui/material/Button';
 import Header from "../../components/Header";
+import { useNavigate } from "react-router-dom";
+import Box from '@mui/material/Box';
+
+
 
 
 export default function Home(){
+  const navigate = useNavigate();
 
+  function handleClick(event: React.MouseEvent<HTMLButtonElement>) {
+    event.preventDefault();
+    navigate('/criar-projeto/');
+  }
+  
   return (
     <>
     <Header/>
+    <Box sx={{textAlign: 'center', alignItems: 'center', height: 260, mx: 'auto', my: { xs: 5, sm: 10 } }}>
+      <Button variant="contained" type="submit" onClick={handleClick}>Criar Projeto</Button>
+    </Box>
     </>
   )
 }
