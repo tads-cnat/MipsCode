8f7d5749c7112fd624cb079b93e08620b1e467e4
heloisemacena <heloisemacena@gmail.com>
2023-05-08 21:07:16 -0300

    :lipstick: Adicionando paleta dark

--- frontend/src/assets/themes/dark.ts
+++ frontend/src/assets/themes/dark.ts
@@ -1,50 +1,47 @@
 import { createTheme } from '@mui/material';
-import { cyan, yellow } from '@mui/material/colors';
+import { ThemeOptions } from '@mui/material/styles';
 
-export const DarkTheme = createTheme({
+export const DarkTheme: ThemeOptions = createTheme({
   palette: {
     primary: {
-      main: '#141417',
-      dark: '#141417',
-      light: yellow[500],
-      contrastText: '#2EF2AA',
+      light: '#57F4BB',
+      main: '#2EF2AA',
+      dark: '#20A976',
+      contrastText: '#000000',
     },
     secondary: {
-      main: cyan[500],
-      dark: cyan[400],
-      light: cyan[300],
+      light: '#E04653',
+      main: '#D91828',
+      dark: '#97101C',
       contrastText: '#ffffff',
     },
+    error: {
+      light: '#E57373',
+      main: '#F44336',
+      dark: '#D32F2F',
+      contrastText: '#ffffff',
+    },
+    warning: {
+      light: '#FFB74D',
+      main: '#FFA726',
+      dark: '#F57C00',
+      contrastText: '#000000',
+    },
+    info: {
+      light: '#4FC3F7',
+      main: '#29B6F6',
+      dark: '#0288D1',
+      contrastText: '#000000',
+    },
+    success: {
+      light: '#81C784',
+      main: '#66BB6A',
+      dark: '#388E3C',
+      contrastText: '#000000',
+    },
     background: {
-      paper: 'white',
-      default: '#202124',
+      paper: '#333333',
+      default: '#1A1B1F',
     }
   }
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import { createTheme } from '@mui/material';
-
-// export const DarkTheme = createTheme({
-//   palette: {
-//     primary: {
-//       main: '#141417',
-//       contrastText: '#2EF2AA',
-//     },
-//   }
-// });
