a8d4281a4119dcba6a8bfc68b4f2bdae1bbb3c97
DANIELA FARIA DE LIMA <d.faria@academico.ifrn.edu.br>
2023-05-29 12:32:23 -0300

    :lipstick: Atualizações na home

--- frontend/src/features/Home/index.tsx
+++ frontend/src/features/Home/index.tsx
@@ -1,6 +1,10 @@
 import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+
 import { useNavigate } from "react-router-dom";
 import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+
 import Box from '@mui/material/Box';
 import {Header, Footer} from '../../components/';
 import React from 'react';
@@ -15,15 +19,15 @@ export default function Home(){
   }
   
 
-  function handleClickCriar(event: React.MouseEvent<HTMLButtonElement>) {
-    event.preventDefault();
-    navigate('/criar-projeto/');
-  }
+  // function handleClickCriar(event: React.MouseEvent<HTMLButtonElement>) {
+  //   event.preventDefault();
+  //   navigate('/criar-projeto/');
+  // }
 
-  function handleClickVer(event: React.MouseEvent<HTMLButtonElement>) {
-    event.preventDefault();
-    navigate('/ver-projetos/');
-  }
+  // function handleClickVer(event: React.MouseEvent<HTMLButtonElement>) {
+  //   event.preventDefault();
+  //   navigate('/ver-projetos/');
+  // }
 
   function handleClickLogin(event: React.MouseEvent<HTMLButtonElement>) {
     event.preventDefault();
@@ -34,22 +38,33 @@ export default function Home(){
     <>
     <Header/>    
 
+    <Grid container spacing={2} columns={16}  rowSpacing={1}>
 
-    <Card sx={{textAlign: 'center', alignItems: 'center' }}>
-      <Button color='secondary' size='small' variant='text' type='submit' onClick={handleClickCadastro}>Cadastro</Button>
-      <Button color='secondary' size='small' variant='text' type='submit' onClick={handleClickLogin}>Login</Button>
-    </Card>
+      <Grid item xs={8}>
+        <Box >
+          <img src = { banner } alt = "BannerMC" width='800px' height='400px' />
+        </Box>
+      </Grid>
 
-    <Card sx={{textAlign: 'center', alignItems: 'center' }}>
-      <img src = { banner } alt = "BannerMC" width='800px' height='400px' />
-    </Card>
+      <Grid item xs={8}>
+        <Box width='800px' height='400px' display='flex' alignItems='center' justifyContent='center'>
+        <Card>
+          <CardContent>
+            <Box display='flex' alignItems='center' justifyContent='center'>
+              <Button color='secondary' size='small' variant='contained' type='submit' onClick={handleClickCadastro}>Cadastro</Button>
+              {/* <Button color='secondary' size="small" variant="contained" type="submit" onClick={handleClickCriar}>Criar Novo Projeto</Button>
+              <Button color='secondary' size="small" variant="outlined" type="submit" onClick={handleClickVer}>Ver todos os projetos</Button> */}
+            </Box>
+            <Box display='flex' alignItems='center' justifyContent='center'>
+              <Button color='secondary' size='small' variant='contained' type='submit' onClick={handleClickLogin}>Login</Button>
+            </Box>
+          </CardContent>
+        </Card>
+        </Box>
+      </Grid>
 
+    </Grid>
 
-    <Box sx={{textAlign: 'center', alignItems: 'center', height: 260, mx: 'auto', my: { xs: 5, sm: 10 }, '& button': { m: 1 } }}>
-      <Button color='secondary' size="small" variant="contained" type="submit" onClick={handleClickCriar}>Criar Novo Projeto</Button>
-      <Button color='secondary' size="small" variant="outlined" type="submit" onClick={handleClickVer}>Ver todos os projetos</Button>
-
-    </Box>
     <Footer/>
     </>
   )
